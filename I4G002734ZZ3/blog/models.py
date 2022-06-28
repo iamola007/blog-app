eee8d52964158a6ce5c397d4b37e76ab2f256954
Ola Oladapo <78447856+iamola007@users.noreply.github.com>
2022-06-28 23:30:14 +0100

    Update models.py

--- I4G002734ZZ3/blog/models.py
+++ I4G002734ZZ3/blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey(
-        'auth.User', on_delete=models.CASCADE,)
+        get_user_model(), on_delete=models.CASCADE,)
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
 
